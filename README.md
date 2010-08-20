6373e433c2c2d0dc24a9deec2d5efd2cc053f1c3
Andreas Blixt <andreas@blixt.org>
2010-08-20 13:49:28 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# fred.ag
+
+## Information
+
+A very simple site informing the user whether today is Friday (fredag in Swedish.)
+
+## MIT license
+
+This project is licensed under an MIT license.  
+<http://www.opensource.org/licenses/mit-license.php>
+
+Copyright (c) 2010 Andreas Blixt <andreas@blixt.org>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>fredag?</title>
+        <style type="text/css">
+            @-webkit-keyframes party {
+                0% {
+                    -webkit-transform: rotate(0);
+                }
+                25% {
+                    font-size: 150px;
+                    margin-top: -75px;
+                    -webkit-transform: rotate(-15deg);
+                }
+                50% {
+                    font-size: 120px;
+                    margin-top: -60px;
+                    -webkit-transform: rotate(0);
+                }
+                75% {
+                    font-size: 150px;
+                    margin-top: -75px;
+                    -webkit-transform: rotate(15deg);
+                }
+            }
+
+            * {
+                margin: 0;
+                padding: 0;
+            }
+
+            h1 {
+                font-size: 120px;
+                line-height: 1;
+                margin-top: -60px;
+                position: absolute;
+                text-align: center;
+                top: 50%;
+                width: 100%;
+            }
+
+            h1.no {
+                color: #c00;
+                text-shadow: 0 0 70px #c00;
+            }
+
+            h1.yes {
+                -webkit-animation-name: party;
+                -webkit-animation-duration: 1s;
+                -webkit-animation-iteration-count: infinite;
+                -webkit-animation-timing-function: linear;
+                color: #0a0;
+                text-shadow: 0 0 70px #0a0;
+            }
+
+            html {
+                background: #111;
+            }
+        </style>
+    </head>
+    <body>
+<?php if (date('w') == 5): ?>
+        <h1 class="yes">fredag!</h1>
+<?php else: ?>
+        <h1 class="no">:(</h1>
+<?php endif; ?>
+    </body>
+</html>
