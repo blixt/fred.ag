e843790e9ce7c617793e12a64f3cd094320ba536
Andreas Blixt <andreas@blixt.org>
2010-08-20 16:21:07 +0200

    Added Google Analytics tracking.

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include('settings.php');
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -57,6 +60,16 @@
                 background: #111;
             }
         </style>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', '<?php echo ANALYTICS_ID; ?>']);
+            _gaq.push(['_trackPageview']);
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
     </head>
     <body>
 <?php if (date('w') == 5): ?>
