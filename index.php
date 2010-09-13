9e6c2b43ea9e9ef2227457f33e43b23cb5bef3dc
Andreas Blixt <andreas@blixt.org>
2010-09-14 00:14:10 +0200

    Fixed all known bugs and made the site available on non-Fridays again.

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
-function not_found() {
-    header('HTTP/1.0 404 Not Found');
-    exit;
-}
-
-$year = date('Y');
+// Get the ISO 8601 week year, number and day. The year may differ from the
+// Gregorian calendar year since ISO 8601 years only consist of whole weeks,
+// which may be a bit confusing.
+$year = date('o');
 $week = date('W');
+$day = date('N');
 
 $request = $_SERVER['REQUEST_URI'];
-if (preg_match('#^/(\\d+)/(\\d+)$#', $request, $match)) {
-    // Fridays that haven't happened yet will return 404.
-    if ($match[1] > $year || ($match[1] == $year && $match[2] > ($day >= 5 ? $week : $week - 1))) {
-        not_found();
+if (preg_match('#^/(\\d{4})/(\\d{2})$#', $request, $match)) {
+    // Invalid values and Fridays that haven't happened yet will return 404.
+    if (($match[1] < 1970 || $match[1] > $year) ||
+        ($match[2] < 1 || $match[2] > 53) ||
+        ($match[1] == $year && $match[2] > ($day >= 5 ? $week : $week - 1)))
+    {
+        header('HTTP/1.0 404 Not Found');
+        exit;
     }
 
     // Change values according to requested year and week.
@@ -19,28 +22,20 @@ if (preg_match('#^/(\\d+)/(\\d+)$#', $request, $match)) {
     $week = $match[2];
     $day = 5;
 } else if ($request != '/') {
-    // Return 404 for invalid paths.
-    not_found();
-} else {
-    $day = date('w');
-}
-
-// Return 404 for invalid values and non-Fridays.
-if ($year < 0 || $week < 1 || $week > 53 || $day != 5) not_found();
-
-// Generate a path for current year/week and make sure the browser is on that path.
-$path = sprintf('/%d/%d', $year, $week);
-if ($request != $path) {
-    header('Location: http://fred.ag' . $path, true, 302);
+    header('HTTP/1.0 404 Not Found');
     exit;
 }
 
 // Get a timestamp for the Friday of the specified date.
-// Count from '0104' because January 4th is always in week 1 (according to ISO 8601).
-// FIXME: Does not work correctly for week 53: http://fred.ag/2009/53
-$time = strtotime($year . '0104 +' . ($week - 1) . ' weeks');
-$timeDay = date('w', $time);
-$time = strtotime(($timeDay <= 5 ? '+' : '') . (5 - $timeDay) . ' days', $time);
+$time = strtotime("$year-W$week-5");
+
+// Generate a path for current year/week and make sure the browser is on that
+// path.
+$path = ($day == 5) ? date('/o/W', $time) : '/';
+if ($request != $path) {
+    header('Location: http://fred.ag' . $path, true, 302);
+    exit;
+}
 
 $months = array(
     'January' => 'januari',
@@ -56,7 +51,6 @@ $months = array(
     'November' => 'november',
     'December' => 'december'
 );
-$friday = 'Fredagen den ' . strtr(date('j:\\e F, Y (\\v\\e\\c\\k\\a W)', $time), $months);
 
 include('settings.php');
 ?>
@@ -64,13 +58,17 @@ include('settings.php');
 <html>
 <head>
     <meta charset="UTF-8">
+<?php if ($day == 5): ?>
     <title>fredag</title>
     <meta property="og:title" content="fredag">
     <meta property="og:type" content="activity">
     <meta property="og:url" content="http://fred.ag<?php echo $path; ?>">
     <meta property="og:site_name" content="fred.ag">
-    <meta property="og:description" content="<?php echo $friday; ?>">
+    <meta property="og:description" content="<?php echo 'Fredagen den ' . strtr(date('j:\\e F, Y (\\v\\e\\c\\k\\a W)', $time), $months); ?>">
     <meta property="fb:app_id" content="<?php echo FACEBOOK_APP_ID ?>">
+<?php else: ?>
+    <title>inte fredag</title>
+<?php endif; ?>
     <style type="text/css">
         * {
             margin: 0;
@@ -87,16 +85,24 @@ include('settings.php');
         }
 
         h1 {
-            color: #0a0;
             font-family: serif;
             font-size: 140px;
             line-height: 1;
             margin-top: -60px;
             position: absolute;
             text-align: center;
-            text-shadow: 0 0 70px #0a0;
             top: 50%;
             width: 100%;
+        }
+
+        h1.no {
+            color: #c00;
+            text-shadow: 0 0 70px #c00;
+        }
+
+        h1.yes {
+            color: #0a0;
+            text-shadow: 0 0 70px #0a0;
         }
 
         html {
@@ -123,7 +129,7 @@ include('settings.php');
             position: absolute;
         }
     </style>
-    <script type="text/javascript">
+    <script>
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', '<?php echo ANALYTICS_ID; ?>']);
         _gaq.push(['_trackPageview']);
@@ -135,24 +141,28 @@ include('settings.php');
     </script>
 </head>
 <body>
-<div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function () {
-        FB.init({appId: '<?php echo FACEBOOK_APP_ID ?>', status: true, cookie: true, xfbml: true});
-    };
+<?php if ($day == 5): ?>
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function () {
+            FB.init({appId: '<?php echo FACEBOOK_APP_ID ?>', status: true, cookie: true, xfbml: true});
+        };
 
-    (function() {
-        var e = document.createElement('script');
-        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-        e.async = true;
-        document.getElementById('fb-root').appendChild(e);
-    }());
-</script>
-    <h1>fredag!</h1>
+        (function () {
+            var e = document.createElement('script');
+            e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+            e.async = true;
+            document.getElementById('fb-root').appendChild(e);
+        })();
+    </script>
+    <h1 class="yes">fredag!</h1>
     <div id="like">
         <p>It's party time!!! Alla gillar fredagar, eller?</p>
         <p><fb:like colorscheme="dark"></fb:like></p>
     </div>
+<?php else: ?>
+    <h1 class="no">:(</h1>
+<?php endif; ?>
     <p id="open-source">Är du en nörd? Bidra! <a href="http://github.com/blixt/fred.ag">fred.ag @ GitHub</a></p>
 </body>
 </html>
